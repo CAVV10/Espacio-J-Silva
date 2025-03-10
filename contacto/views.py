cb1ccb88320d3113711718e72259963ed1ccfff1
CarlosVargas <vargasvillanuevacarlosalberto@gmail.com>
2025-03-10 10:11:28 -0500

    Fix: cambio email por username en authenticate

--- contacto/views.py
+++ contacto/views.py
@@ -89,7 +89,7 @@ def login_view(request):
         print(f"Intentando iniciar sesión con email: {email} y contraseña: {password}")
 
         try:
-            user = authenticate(request, email=email, password=password)
+            user = authenticate(request, username=email, password=password)
 
             if user is not None:
                 login(request, user)
