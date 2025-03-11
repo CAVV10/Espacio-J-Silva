afa72f6ab342534717446a3581b094855ab74776
CarlosVargas <vargasvillanuevacarlosalberto@gmail.com>
2025-03-11 08:52:23 -0500

    CARRITO DE COMPRAS

---
+++ contacto/templatetags/carrito_extras.py
@@ -0,0 +1,13 @@
+
+from django import template
+
+from contacto.models import Carrito
+@register.filter
+def carrito_cantidad(user):
+    if user.is_authenticated:
+        try:
+            carrito = Carrito.objects.get(usuario=user)
+            return sum(item.cantidad for item in carrito.itemcarrito_set.all())
+        except Carrito.DoesNotExist:
+            return 0
+    return 0
